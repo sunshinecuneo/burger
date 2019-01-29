1581933c1e7a5eb51d3e5f8bd220f44169c7da77
Tiffany Cuneo <tiffanycuneo@gmail.com>
2019-01-29 15:35:36 -0700

    heroku push2

---
+++ heroku_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE burgers_db;
+
+USE `bs2wx6h7hyo5wdk5`;
+
+INSERT INTO Burgers (burger_name, createdAt, updatedAt) VALUES ("Big Kahuna", '1970-01-01 00:00:01', '1970-01-01 00:00:01'), ("Veggie Burger", '1970-01-01 00:00:01', '1970-01-01 00:00:01'), ("Baconator", '1970-01-01 00:00:01', '1970-01-01 00:00:01');
